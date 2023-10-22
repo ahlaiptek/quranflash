b657ffa3838ea04ba5421de5c7df4103752502c1
ahlaiptek <iptekahla@gmail.com>
2023-10-22 21:08:27 +0700

    Menambahkan sinkronasi data csv ke products

---
+++ src/js/products.js
@@ -0,0 +1,31 @@
+function csv_to_json(csv) {
+	const lines = csv.split("\n");
+	const result = [];
+
+	for (let i = 0; i < lines.length; i++) {
+		let buffer = lines[i].split(",");
+		result.push(buffer)
+	}
+
+  	return result;
+}
+
+async function run() {
+	let result = await (await fetch("/data.csv")).text();
+	result = csv_to_json(result);
+
+	const produk_element = document.getElementById("produk");
+	
+	for (const x of result) {
+		let html = `<div class="barang">
+            <img src="gambar-produk2.jpg" alt="Gambar produk">
+            <h2>${x[0]}</h2>
+            <p>Deskripsi Produk 2.</p>
+            <p>Harga: ${x[1]}</p>
+        </div>`;
+        html = parseHTML(html);
+        produk_element.appendChild(html);
+	}
+}
+
+run();
