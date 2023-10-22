f2d70c20ea929e4d76b6c47e39b7c213c0e0c306
ahlaiptek <iptekahla@gmail.com>
2023-10-22 17:01:57 +0700

    Menambahkan halaman products dan tombol wa melayang serta perbaikan lainnya

---
+++ src/js/scripts.js
@@ -0,0 +1,13 @@
+function parseHTML(html) {
+    const t = document.createElement('template');
+    t.innerHTML = html;
+    return t.content;
+}
+
+async function load_file_html(target, url) {
+    let result = await (await fetch(url)).text();
+    result = parseHTML(result);
+    target.appendChild(result);
+}
+
+load_file_html(document.body, '/partials/wa_melayang.html')
